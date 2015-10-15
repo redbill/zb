9a8eb6f24bd5c6417764c35cf4a211a1aede2629
EvilSophie <1064622006@qq.com>
2015-10-15 17:08:53 +0800

    去掉多余代码

--- src/main/java/com/bianlide/zb/cms/controller/CmsController.java
+++ src/main/java/com/bianlide/zb/cms/controller/CmsController.java
@@ -1,22 +1,7 @@
 package com.bianlide.zb.cms.controller;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import cn.com.hugedata.web.fsm.user.model.UserInfo;
 
 @RestController  
 public class CmsController {
